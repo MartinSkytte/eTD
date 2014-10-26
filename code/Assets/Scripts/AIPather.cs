d1bffdb14abd6eda5780b05cee27c83bad51ad1b
MartinSkytte <m.s.s@keepahead.dk>
2014-10-26 17:42:04 +0100

    TowerDefence
    Added Pathing and old school TD level. One creep finding its way through
    the level.

---
+++ code/Assets/Scripts/AIPather.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class AIPather : MonoBehaviour {
+
+	public Transform target;
+	public float speed = 10f;
+	public float maxWaypointDistance = 2f;
+
+	private Seeker seeker;
+	private Path path;
+	private int currentWaypoint;
+
+	void Start() {
+		seeker = GetComponent<Seeker> ();
+		seeker.StartPath (transform.position, target.position, OnPathComplete);
+	}
+
+	public void OnPathComplete(Path p) {
+		if (!p.error) {
+				path = p;
+				currentWaypoint = 0;
+		} else {
+			Debug.Log(p.error);
+		}
+	}
+
+	void FixedUpdate() {
+		if (path == null)
+			return;
+
+		if (currentWaypoint >= path.vectorPath.Count)
+			return;
+
+		Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized * speed * Time.fixedDeltaTime;
+		transform.position = path.vectorPath [currentWaypoint];
+
+		if(Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]) < maxWaypointDistance)
+			currentWaypoint++;
+	}
+}
