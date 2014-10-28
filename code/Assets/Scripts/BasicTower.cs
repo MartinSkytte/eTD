4b766df1b10e6a8da6f18700c300cdd1eb7607e6
Mike Karstensen <mikbekar@gmail.com>
2014-10-28 10:59:10 +0100

    made simple tower

---
+++ code/Assets/Scripts/BasicTower.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicTower : MonoBehaviour {
+
+	public GameObject projectile;
+	public float reloadTime = 1;
+	public Transform target;
+
+	private float nextFireTime;
+
+
+
+
+	void OnTriggerEnter(Collider c){
+		target = c.gameObject.transform;
+	}
+
+	void OnTriggerExit(Collider c){
+		if (c.gameObject.transform == target) {
+			target = null;	
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target){
+				if (Time.time >= nextFireTime) {
+						nextFireTime = Time.time + reloadTime;
+						this.transform.rotation = Quaternion.LookRotation(target.localPosition);
+						Instantiate (projectile, transform.position, transform.rotation);
+				}	
+		}
+	}
+
+
+}
