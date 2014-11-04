07a770f3e060a6b29b787a4e6c0b4ed939aa8326
MartinSkytte <m.s.s@keepahead.dk>
2014-11-04 17:02:56 +0100

    Added a new Basic unit, and updated the spawner
    Added a new Basic unit, and updated the spawner.
    The spawner is updated so it now can spawn units

--- code/Assets/Scripts/spawner.cs
+++ code/Assets/Scripts/spawner.cs
@@ -4,22 +4,32 @@ using System.Collections.Generic;
 
 public class spawner : MonoBehaviour {
 
-	private List<GameObject> wave;
 	public int currentWave;
 	private LevelInfoList infoList;
-	private List<LevelInfoList.LevelInfo> list;
-	private LevelInfoList.LevelInfo levelInfo;
+	private List<LevelInfoList.LevelInfo> levelInfo;
 
 	// Use this for initialization
 	void Start () {
+		infoList = (LevelInfoList)gameObject.GetComponent<LevelInfoList> ();
+		levelInfo = infoList.levelsInfo;
+		InitiateNewWave (currentWave);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
-	private void InitiateWave(int waveNumber) {
+	private void InitiateNewWave(int waveNumber) {
+		for (int i = 0; i < levelInfo[waveNumber].amount; i++) {
+			GameObject creep = levelInfo[waveNumber].creep;
+			Debug.Log(creep.ToString());
+			AIPather path = creep.AddComponent<AIPather>();
+			path.target = levelInfo[waveNumber].target;
+			path.speed = 5;
+			path.maxWaypointDistance = 0.5f;
 
+			Instantiate(creep, transform.position, transform.rotation);
+		}
 	}
 }
