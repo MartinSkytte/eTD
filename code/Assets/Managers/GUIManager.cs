0cb4192c8a908051efc6568431a175802d4fc4d3
FrenchDane <christian_nicolaisen@hotmail.com>
2014-11-11 15:11:10 +0100

    The game is now working!!

--- code/Assets/Managers/GUIManager.cs
+++ code/Assets/Managers/GUIManager.cs
@@ -3,23 +3,18 @@ using System.Collections;
 
 public class GUIManager : MonoBehaviour {
 
-	public GUIText CurrentNumberText, GoalNumberText, GameWonText, GameOverText, InstructionsText, MatchText, TheGameText;//, TimerText;
+	public GUIText CurrentNumberText, GoalNumberText, InstructionsText, TheGameText, TimerText, CreditsText;
 
 	private static GUIManager instance;
 
 	// Use this for initialization
 	void Start () {
 		instance = this;
-		GameEventManager.GameStart += GameStart;
-		GameEventManager.GameOver += GameOver;
+
 		GameEventManager.GameWon += GameWon;
-		TheGameText.text = "eTD";
-		//SetMidText("Start");
-		//TheGameText.text= "eTD The Game";
-		//sTheGameText.text="eTD The Game";
-		//GameOverText.enabled = false;
-		//GameWonText.enabled = false;
-		//TheGameText.text = "eTD";
+		GameEventManager.GameOver += GameOver;
+		GameEventManager.GameStart += GameStart;
+		SetMidText("Start");
 	}
 
 	// Update is called once per frame
@@ -30,6 +25,7 @@ public class GUIManager : MonoBehaviour {
 	}
 
 	public static void SetMidText(string tex){
+		instance.TheGameText.enabled = true;
 		switch (tex) {
 		case "Start":
 			instance.TheGameText.text="eTD The Game";
@@ -39,6 +35,9 @@ public class GUIManager : MonoBehaviour {
 			break;
 		case "Lost":
 			instance.TheGameText.text="Game Over";
+			break;
+		case "Nothing":
+			instance.TheGameText.enabled = false;
 			break;
 		}
 	}
@@ -52,39 +51,29 @@ public class GUIManager : MonoBehaviour {
 		instance.GoalNumberText.text = goalN.ToString();
 	}
 
-//	public static void SetCurrentTime(int currentT){//countdown timer in seconds
-	//	instance.TimerText.text = currentT.ToString();
-	//}
+	public static void SetCurrentTime(int currentT){//countdown timer in seconds
+		instance.TimerText.text = currentT.ToString();
+	}
+
+	public static void SetCredits(int credits){
+		instance.CreditsText.text = credits.ToString ();
+	}
 
 	private void GameOver(){
-		//SetMidText ("Lost");
-		//GameWonText.enabled = false;
-		//GameOverText.enabled = true;
-		TheGameText.enabled = true;
-		TheGameText.text="Game Over";
+		SetMidText ("Lost");
 		InstructionsText.enabled = true;
 		enabled = true;
 
 	}
-
-
+	
 	private void GameStart(){
-		//TheGameText.enabled = false;
-		TheGameText.enabled = false;
-		//GameWonText.enabled = false;
-		//GameOverText.enabled = false;
-		//TheGameText.enabled = false;
+		SetMidText("Start");
 		InstructionsText.enabled = false;
 		enabled = false;
-
 	}
 
 	private void GameWon(){
-		TheGameText.enabled = true;
 		SetMidText("Win");
-		//TheGameText.text="You Win";
-		//GameWonText.enabled = true;
-		//GameOverText.enabled = false;
 		InstructionsText.enabled = true;
 		enabled = true;
 	}
