d17ad39f86e93e7b6ccece7d085512c81fbbbb64
MartinSkytte <m.s.s@keepahead.dk>
2014-12-18 00:29:25 +0100

    added music generation to the project

---
+++ code/Assets/Scripts/Music.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using LibPDBinding;
+
+public class Music : MonoBehaviour {
+
+	private float nextActionTime = 0.0f; 
+	public float period = 1f;
+
+	// Use this for initialization
+	void Start () {
+		LibPD.SendMessage ("Load-current-melody", "bang");
+		LibPD.SendMessage ("bpm", "float", 120f);
+		LibPD.SendMessage ("musicVolume", "float", 1f);
+	}
+}
